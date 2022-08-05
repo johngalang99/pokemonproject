49a7ad60067d1720f3119e8f239494144adf65ad
immangalang <87266699+immangalang@users.noreply.github.com>
2022-08-05 11:05:44 +0800

    transfer hardcoded favorite pokemons into another file

--- src/components/Pokemons/index.jsx
+++ src/components/Pokemons/index.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { favoritePokemons } from '../../data/favoritePokemons';
 import PokemonRow from './PokemonRow';
 
 const Pokemons = () => {
+  const [
+    poke1,
+    poke2,
+    poke3,
+    poke4,
+    poke5,
+    poke6,
+    poke7,
+    poke8,
+    poke9,
+    poke10,
+  ] = favoritePokemons;
+
   return (
     <div className="flex flex-col items-center overflow-scroll">
-      <PokemonRow name1={'charizard'} name2={'pidgeot'} />
-      <PokemonRow name1={'lapras'} name2={'krabby'} />
-      <PokemonRow name1={'pikachu'} name2={'alakazam'} />
-      <PokemonRow name1={'kabutops'} name2={'snorlax'} />
-      <PokemonRow name1={'mewtwo'} name2={'dragonite'} />
+      <PokemonRow name1={poke1} name2={poke2} />
+      <PokemonRow name1={poke3} name2={poke4} />
+      <PokemonRow name1={poke5} name2={poke6} />
+      <PokemonRow name1={poke7} name2={poke8} />
+      <PokemonRow name1={poke9} name2={poke10} />
     </div>
   );
 };
---
+++ src/data/favoritePokemons.js
@@ -0,0 +1,12 @@
+export const favoritePokemons = [
+  'charizard',
+  'pidgeot',
+  'lapras',
+  'krabby',
+  'pikachu',
+  'alakazam',
+  'kabutops',
+  'snorlax',
+  'mewtwo',
+  'dragonite',
+];
